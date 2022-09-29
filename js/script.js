5752409e38b251d322eff62b305481b27d69739e
Egor <vegormak7@gmail.com>
2022-09-29 19:23:20 +0300

    second mod

--- js/script.js
+++ js/script.js
@@ -11,15 +11,42 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-const lastFilmMark = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastFilm] = lastFilmMark;
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10  && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Error');
+}
 
-const lastFilm1 = prompt('Один из последних просмотренных фильмов?', '');
-const lastFilmMark1 = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+    const lastFilmMark = +prompt('На сколько оцените его?', '');
+    if (lastFilm != null && lastFilmMark != null && lastFilm != '' && lastFilmMark != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = lastFilmMark;
+        console.log('Done');
+    } else {
+        i--;
+        console.log('Error');
+    } 
+}
 
-personalMovieDB.movies[lastFilm1] = lastFilmMark1;
+
+// for (let i = 1; i < 3; i++) {
+//     let lastFilm;
+//     let lastFilmMark;
+//     while (!lastFilm || lastFilm.length > 50) {
+//         lastFilm = prompt( i + '- Один из последних просмотренных фильмов?', '');
+//     }
+//     while (!lastFilmMark || lastFilm.length > 50) {
+//         lastFilmMark = +prompt(i + '- На сколько оцените его?', '');
+//     }
+//     personalMovieDB.movies[lastFilm] = lastFilmMark;
+// }
 
 
 console.log(personalMovieDB);
+
